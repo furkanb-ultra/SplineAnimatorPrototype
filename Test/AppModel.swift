c91f971ebabddffcdf1d5d953979505bebde7a05
furkanb-ultra <furkan.b@ultra.site>
2025-05-21 10:51:09 +0200

    Initial Commit

---
+++ Test/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  Test
+//
+//  Created by Furkan on 21/05/25.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ Test/TestApp.swift
@@ -0,0 +1,33 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Furkan on 21/05/25.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
